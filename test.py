d1bb3ac9cb1f965d06cab500abab44e15597bee6
Daniel <daniel.baker1608@gmail.com>
2020-11-29 15:50:39 -0500

    Added some simple DB interfacing

--- test.py
+++ test.py
@@ -66,4 +66,4 @@ def research():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8007)
--- testLog.py
+++ testLog.py
@@ -1,2 +1,25 @@
-def testLog():
-    print("TEST")
+# Module Imports
+import mariadb
+import sys
+
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user="cs307-group07@localhost",
+        password="q6m527HgKJuLStZD",
+        host="0.0.0.0",
+        database="cs307-group07-DB"
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM users")
+
+# Print Result-set
+for (user) in cur:
+    print(user)
