aa00220dc425d557b770755651e2a90070845c2c
Daniel <daniel.baker1608@gmail.com>
2020-12-03 15:40:13 -0500

    Cleaned up test.py. Added content of General Info to DB. Content css tweaks

--- test.py
+++ test.py
@@ -6,18 +6,6 @@ import SQL_Interface
 
 app = Flask(__name__)
 
-# Open database connection
-db = pymysql.connect("localhost","cs307-group07","q6m527HgKJuLStZD","cs307-group07-DB" )
-
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
-
-# Query content of 'teaching' slide show in DB
-cursor.execute("SELECT content FROM teaching")
-rows = cursor.fetchall()
-teaching = Markup(rows[0])
-
-
 # pass usualTeaching if user doesn't have permission and editTeaching2 if he does
 usualTeaching = Markup("""teaching<span style="color: red">@CS</span>""")
 editTeaching2 = Markup("""<a href="/editTeaching" style="text-decoration: none; color: white"><span style="color: red">Edit Teaching</span></a>""")
@@ -35,18 +23,17 @@ def editTeaching():
     if request.method == "POST":
         newteaching = request.form["editArea"]
         # add to database
-        #teaching = Markup(newteaching)
-        #update_string = """UPDATE teaching
-        #SET content='{0}'
-        #WHERE ID=0"""
-        #update_string = update_string.format(teaching)
-
-        print(newteaching)
+        teaching = Markup(newteaching)
+        update_string =  """UPDATE teaching
+                            SET content='{0}'
+                            WHERE ID=0"""
+        update_string = update_string.format(teaching)
+        print("teaching content has been updated")
         return render_template("html/edit/editConfirmation.html")
     else:
         query_string = "SELECT content FROM teaching"
         data = SQL_Interface.query(query_string)
-        teaching = data[0]
+        teaching = data[0][0]
         return render_template("html/edit/editTeaching.html", teaching=teaching)
 
 @app.route("/login", methods=["POST", "GET"])
@@ -61,9 +48,16 @@ def attempt_login():
         #testLog()
     return render_template("html/login/login.html")
 
-@app.route('/prospective-menu')
+@app.route('/prospective')
 def prospective():
     return render_template("html/prospective/menu-prospective.html")
+
+@app.route('/general_info')
+def gen_info():
+    query_string = "SELECT content FROM prospective WHERE ID='generalInfo'"
+    data = SQL_Interface.query(query_string)
+    content = data[0][0]
+    return render_template("html/prospective/gen-info.html", content=content)
 
 @app.route('/people-menu')
 def people():
@@ -75,17 +69,12 @@ def academic():
 
 @app.route('/news')
 def news():
-    teaching = Markup("<h1>HELLOOOOOOOOOO</h1>")
-    update_string = """UPDATE teaching
-    SET content='{0}'
-    WHERE ID=0"""
-    update_string = update_string.format(teaching)
-    #SQL_Interface.update(update_string)
     return render_template("html/news/news.html")
 
 @app.route('/about-menu')
 def about():
     return render_template("html/about/menu-about.html")
+
 @app.route('/research')
 def research():
     return render_template("html/research/research.html")
