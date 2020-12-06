17155beb882dd5b96528dba96395373f5f8c539f
Rui <rui.guo2@mail.mcgill.ca>
2020-12-06 14:10:58 -0500

    Added Searching

---
+++ static/scripts/search.js
@@ -0,0 +1,54 @@
+let searchFieldShow = false;
+
+function searchField(){
+    if (!searchFieldShow){
+        let search = document.getElementById("searchBar");
+        search.style.display = "block";
+        searchFieldShow = true;
+    }
+    else{
+        let search = document.getElementById("searchBar");
+        search.style.display = "none";
+        searchFieldShow = false;
+    }
+}
+
+function go(){
+    let input = document.getElementById("searchField").value;
+    if (input.toLowerCase() === "prospective"){
+        window.location.href = "/prospective";
+        console.log("prospective");
+    }
+    if (input.toLowerCase() === "academic"){
+        window.location.href = "/academic";
+        console.log("academic");
+    }
+    if (input.toLowerCase() === "research"){
+        window.location.href = "/research";
+        console.log("research");
+    }
+    if (input.toLowerCase() === "donate"){
+        window.location.href = "/donate";
+        console.log("donate");
+    }
+    if (input.toLowerCase() === "login"){
+        window.location.href = "/login";
+        console.log("login");
+    }
+    if (input.toLowerCase() === "people"){
+        window.location.href = "/people";
+        console.log("people");
+    }
+    if (input.toLowerCase() === "news"){
+        window.location.href = "/news";
+        console.log("news");
+    }
+    if (input.toLowerCase() === "employment"){
+        window.location.href = "/employment";
+        console.log("employment");
+    }
+    if (input.toLowerCase() === "about"){
+        window.location.href = "/about";
+        console.log("about");
+    }
+}
